ef964f6444d8a1ad0c311ef291ca7932d6dafd4a
grandtheft-spacehip <justdizon71@gmail.com>
2016-06-21 09:36:16 -0700

    Added new post function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  get '/posts/new' => 'posts#new'
   get '/posts' => 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
