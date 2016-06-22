467e36cbfa51d8d156096ad9d8ffe26886d9833a
grandtheft-spacehip <justdizon71@gmail.com>
2016-06-21 17:42:51 -0700

    Added create functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  post '/posts' => 'posts#create'
   get '/posts/new' => 'posts#new'
   get '/posts' => 'posts#index'
+
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
