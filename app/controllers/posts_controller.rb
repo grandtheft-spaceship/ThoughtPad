a0802926a2aca33fbfe6ace968c5f54372c1c7ba
grandtheft-spacehip <justdizon71@gmail.com>
2016-06-23 09:54:01 -0700

    Added update function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,14 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
-  
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+
+    redirect_to post_path(@post)
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/posts/new' => 'posts#new', :as => 'new_post'
   get '/posts/:id' => 'posts#show', :as => 'post'
   get '/posts/:id/edit' => 'posts#edit', :as => 'edit_post'
+  patch '/posts/:id' => 'posts#update'
 
   root 'posts#index'
 
