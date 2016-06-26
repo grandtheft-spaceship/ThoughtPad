27e61710a77537ce38dbd536b6fde804afeb73ed
grandtheft-spacehip <justdizon71@gmail.com>
2016-06-25 20:00:26 -0700

    Created comments table and set up relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623234943) do
+ActiveRecord::Schema.define(version: 20160626023417) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
